b650d6b38a8f5ebbb07287673947a5180c2f698d
Иван <dreamscometrueivan97@gmail.com>
2024-04-27 21:50:41 +0300

    1

---
+++ src/main/java/com/webhomework/webemployee/WebEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.webhomework.webemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebEmployeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebEmployeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/webhomework/webemployee/WebEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webhomework.webemployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebEmployeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
